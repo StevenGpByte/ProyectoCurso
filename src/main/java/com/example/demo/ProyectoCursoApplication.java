bb10c33d105a859ebf0ee7cfcabc5b921ef4aa80
sdev <sgarcia@bytesw.com>
2023-04-03 12:51:40 -0600

    first commit

---
+++ src/main/java/com/example/demo/ProyectoCursoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoCursoApplication.class, args);
+	}
+
+}
